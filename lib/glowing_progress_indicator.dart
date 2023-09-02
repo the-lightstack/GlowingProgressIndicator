8ae47ad14bf8980264dcfd3f6cc4c65dfbbb61cc
bottleInALightning <lightningbottle@protonmail.com>
2023-09-02 18:37:26 +0200

    2: glowing ring

--- lib/glowing_progress_indicator.dart
+++ lib/glowing_progress_indicator.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -11,15 +12,27 @@ class GlowingProgressIndicator extends StatelessWidget {
     return SizedBox(
       width: 300,
       height: 300,
-      child: CustomPaint(
-        painter: _CirclePainter(progress),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          CustomPaint(
+            painter: _OuterGlowPainter(progress),
+          ),
+          BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+              child: Container()),
+          CustomPaint(painter: _InnerLightTubePainter(progress)),
+          BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+              child: Container()),
+        ],
       ),
     );
   }
 }
 
-class _CirclePainter extends CustomPainter {
-  _CirclePainter(this.progress);
+class _OuterGlowPainter extends CustomPainter {
+  _OuterGlowPainter(this.progress);
 
   final double progress;
 
@@ -28,14 +41,39 @@ class _CirclePainter extends CustomPainter {
     final fullCanvasRect = Rect.fromLTWH(0, 0, size.width, size.height);
     final strokePaint = Paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 20
-      ..color = Colors.greenAccent;
+      ..strokeWidth = 15
+      ..color = Colors.blue.shade500
+      ..strokeCap = StrokeCap.round;
     canvas.drawArc(
         fullCanvasRect, -.5 * pi, 2 * pi * progress, false, strokePaint);
   }
 
   @override
-  bool shouldRepaint(covariant _CirclePainter oldDelegate) {
+  bool shouldRepaint(covariant _OuterGlowPainter oldDelegate) {
+    return progress != oldDelegate.progress;
+  }
+}
+
+class _InnerLightTubePainter extends CustomPainter {
+  _InnerLightTubePainter(this.progress);
+
+  final double progress;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final fullCanvasRect = Rect.fromLTWH(0, 0, size.width, size.height);
+
+    final whiteSmall = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5
+      ..color = Colors.blue.shade100
+      ..strokeCap = StrokeCap.round;
+    canvas.drawArc(
+        fullCanvasRect, -.5 * pi, 2 * pi * progress, false, whiteSmall);
+  }
+
+  @override
+  bool shouldRepaint(covariant _InnerLightTubePainter oldDelegate) {
     return progress != oldDelegate.progress;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: GlowingProgressIndicator(progress: .8),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color(0xff050a30),
+            Color(0xff000c66),
+            Color.fromARGB(255, 7, 7, 170),
+          ], begin: Alignment.topRight)),
+          child: const Center(
+            child: GlowingProgressIndicator(progress: .5),
+          ),
         ),
       ),
     );
